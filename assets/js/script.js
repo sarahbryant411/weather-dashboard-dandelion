f35d4f3262e739e0632c97db5f25c68fdca11eb8
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-26 14:49:33 -0500

    restarted on javascript and added the variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
 
-
+var apiKey = 'e38f1f3c83719cf96fbce49bc31a4fdd'
+var apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+var searchForm = document.getElementById('search-form');
+var locationElement = document.getElementById('location');
+var temperatureElement = document.getElementById('temperature');
+var conditionsElement = document.getElementById('condition');
