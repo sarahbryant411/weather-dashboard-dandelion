ac1a764a2650b870307447fe75b1f25ff3a0fd7d
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-26 22:16:15 -0500

    added the lat,lon geo weather function and a bunch of other random move arounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var apiKey = "e38f1f3c83719cf96fbce49bc31a4fdd";
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather";
+var UrlForecast = "https://api.openweathermap.org/data/2.5/forecast";
+
 var searchForm = document.getElementById("search-form");
 var cityName = document.getElementsByClassName("city-name");
 var temperatureElement = document.getElementsByClassName("temperature");
@@ -19,10 +21,21 @@ searchForm.addEventListener("submit", function (event) {
     fetchWeather(city);
 });
 
-function fetchWeather(city) {
-    var url = apiUrl + "?q=" + city + "&units=imperial&appid=" + apiKey;
+function getGeoWeather(lat, lon) {
+    fetch('http://api.openweathermap.org/data/2.5/forecast?appid=8823600ae11757d74ec67f06b60ca5ef&lat=' + lat + '&lon=' + lon + '&units=imperial')
+        .then(function (response) {
+            return response.json();
+        })
+    }
 
-    fetch(url)
+function fetchWeather(city) {
+    var urlCurrent = apiUrlCurrent + "?q=" + city + "&units=imperial&appid=" + apiKey;
+    var urlForecast = UrlForecast + "?q=" + cityName + "&units=imperial&appid=" + apiKey;
+
+    console.log("urlCurrent: ", urlCurrent);
+    console.log("urlForecast: ", urlForecast);
+    
+    fetch(urlCurrent)
         .then(function (response) {
             if (!response.ok) {
                 throw response.json();
@@ -63,7 +76,22 @@ function fetchWeather(city) {
         .catch(function (error) {
             console.error(error);
         });
+
+        fetch(urlForecast)
+    .then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
+
+    .then(function (data) {
+        console.log(data);
+        console.log("data.main.temp = ", data.main.temp);
+        console.log(data.name);
+    })
 }
+
 
 var cityButtons = document.querySelectorAll(".search-history button");
 cityButtons.forEach((button) => {
@@ -101,6 +129,7 @@ function createWeatherCard(city, temperature, windspeed, humidity) {
 }
 
 var weather = document.getElementById("current-weather");
+
 function createForecastCard(city, temperature, windspeed, humidity) {
     var card = document.createElement("div");
     card.className = "forecast-card";
@@ -131,3 +160,6 @@ function createForecastCard(city, temperature, windspeed, humidity) {
 var forecast = document.getElementById("forecast");
 // var forecastCard = createForecastCard(city, temperature, windspeed, humidity);
 // forecast.appendChild(forecastCard);
+
+
+
