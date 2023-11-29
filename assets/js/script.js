25e51df1f21dcee9e00b1d52b75b86e4b2fd1bd4
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-28 20:32:38 -0500

    restarted js again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,172 +1,90 @@
-var apiKey = "e38f1f3c83719cf96fbce49bc31a4fdd";
-var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather";
-var UrlForecast = "https://api.openweathermap.org/data/2.5/forecast";
+// Import API key from a seperate file
+import {apiKey} from './apiKey.js';
 
-var searchForm = document.getElementById("search-form");
-var cityName = document.getElementsByClassName("city-name");
-var temperatureElement = document.getElementsByClassName("temperature");
-var windspeedElement = document.getElementsByClassName("wind-speed");
-var humidityElement = document.getElementsByClassName("humidity");
+function getWeatherData(city) {
 
-var searchHistory = document.querySelectorAll("search-history");
-var weather = document.getElementById("current-weather");
-var forecast = document.getElementById("forecast");
+    // Define the URL
+    var apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
-document.querySelector(".city-name").textContent = "City Name";
-document.querySelector(".temperature").textContent = "Temperature";
-document.querySelector(".wind-speed").textContent = "Wind Speed";
-document.querySelector(".humidity").textContent = "Humidity";
-
-
-searchForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    var city = event.target[0].value;
-    console.log(city);
-    fetchWeather(city);
-});
-// Function to fetch data based on the latitude and longitude
-
-function getGeoWeather(lat, lon) {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?appid=8823600ae11757d74ec67f06b60ca5ef&lat=' + lat + '&lon=' + lon + '&units=imperial')
+    // Fetch the data
+    fetch(apiUrl)
         .then(function (response) {
+            // Convert the response to JSON format
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            getGeoWeather(data.lat, data.lon);
-        });
-}
-// Function to fetch data based on the city name
-function getCityGeoData() {
-    fetch('https://apiKey.openweathermap.org/geo/1.0/direct?qappid=8823600ae11757d74ec67f06b60ca5ef&limit=1&q=Atlanta')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            getCityGeoData(data[0].lat, data[0].lon);
-        });
-}
-
-function getCurrentWeather(cityName, temperature, windspeed, humidity) {
-    // fetch code goes here
-    fetch(apiUrlCurrent)
-        .then(function (response) {
-            if (!response.ok) {
-                throw response.json();
-            }
-            return response.json();
-        })
-        .then(function (data) {
-            var weatherCard = getCurrentWeather(cityName, temperature, windspeed, humidity);
-            weather.appendChild(weatherCard);
-
-            var weatherContainer = document.querySelector(".weather-container");
-            weatherContainer.appendChild(weatherCard);
+            // Handle the data
+            handleWeatherData(data);
+            fetchForecastData(data.coord.lat, data.coord.lon);
         })
         .catch(function (error) {
-            console.error(error);
+            // If there is any error you will catch them here
+            console.error('Error:', error);
+            alert('Failed to get weather data. Please try again.');
+        });
+}
 
-            function getForecast(cityName, temperature, windspeed, humidity) {
-                // fetch code goes here
-                fetch(UrlForecast)
-                    .then(function (response) {
-                        if (!response.ok) {
-                            throw response.json();
-                        }
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        var forecastData = data.list;
-                        forecastData.forEach(function (day) {
-                            var forecastCard = createForecastCard(cityName, temperature, windspeed, humidity);
-                            forecastContainer.appendChild(forecastCard);
-                        });
-                    })
-                    .catch(function (error) {
-                        console.error(error);
+function handleWeatherData(data) {
+    // Get the elements where you want to display the data
+    var temperatureEl = document.querySelector('#temperature');
+    var conditionEl = document.querySelector('#condition');
+    var locationEl = document.querySelector('#location');
 
-                        function fetchWeather(city) {
-                            var urlCurrent = apiUrlCurrent + "?q=" + city + "&units=imperial&appid=" + apiKey;
-                            var urlForecast = UrlForecast + "?q=" + cityName + "&units=imperial&appid=" + apiKey;
+    // Update the text content of the elements with data from the API
+    temperatureEl.textContent = `${data.main.temp}°F`;
+    conditionEl.textContent = data.weather[0].description;
+    locationEl.textContent = data.name;
+}
 
-                            getCurrentWeather(cityName, temperature, windspeed, humidity);
-                            getForecast(cityName, temperature, windspeed, humidity);
-                        }
+function handleForecastData(data) {
+    // Get the element where you want to display the data
+    var forecastContainerEl = document.querySelector('#forecast-container');
 
-                        var cityButtons = document.querySelectorAll(".search-history button");
-                        cityButtons.forEach((button) => {
-                            button.addEventListener("click", () => {
-                                var city = button.textContent;
-                                fetchWeather(city);
-                            });
-                        });
+    data.list.forEach(item => {
+        var forecastItem = document.createElement('div');
+        forecastItem.textContent = `${new Date(item.dt_txt).toLocaleDateString()} - ${item.main.temp}°F - ${item.weather[0].description}`;
+        forecastContainerEl.appendChild(forecastItem);
+    });
+}
 
-                        function createWeatherCard(city, temperature, windspeed, humidity) {
-                            var card = document.createElement("div");
-                            card.className = "weather-card";
+function fetchForecastData(lat, lon){
+    // Define the URL
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-                            var cityName = document.createElement("h2");
-                            cityName.className = "city-name";
+    // Use the fetch API to get the forecast data
+    fetch(apiUrl)
+        .then(function (response) {
+            // Convert the response to JSON format
+            return response.json();
+        })
+        .then(function (data) {
+            // Handle the data
+            handleForecastData(data);
+        })
+        .catch(function (error) {
+            // If there is any error you will catch them here
+            console.error('Error:', error);
+            alert('Failed to get forecast data. Please try again.');
+        });
+}
+        // Add the event listener to the form
+        document.querySelector('#search-form').addEventListener('submit', function (event) {
+            event.preventDefault();
+            var city = document.querySelector('#city').value;
+            getWeatherData(city);
+        });
+        
 
-                            var span = document.createElement("span");
-                            span.textContent = city;
-                            cityName.appendChild(span);
+// Add event listener to the search history buttons
+var searchHistoryButtons = document.querySelectorAll('.search-history-button');
+searchHistoryButtons.forEach(function(button) {
+    button.addEventListener('click', function(){
+        var city = this.textContent;
+        getWeatherData(city);
+    });
+});
 
-                            var temp = document.createElement("p");
-                            temp.className = "temperature";
-                            temp.textContent = temperature;
-                            card.appendChild(temp);
 
-                            var wind = document.createElement("p");
-                            wind.className = "windspeed";
-                            wind.textContent = windspeed;
-                            card.appendChild(wind);
-
-                            var humidityElement = document.createElement("p");
-                            humidityElement.className = "humidity";
-                            humidityElement.textContent = humidity;
-                            card.appendChild(humidityElement);
-
-                            return card;
-                        }
-
-                        function createForecastCard(city, temperature, windspeed, humidity) {
-                            var card = document.createElement("div");
-                            card.className = "forecast-card";
-
-                            var cityName = document.createElement("h2");
-                            cityName.className = "city-name";
-                            
-                            var span = document.createElement("span");
-                            span.textContent = city;
-                            cityName.appendChild(span);
-
-                            var date = document.createElement("p");
-                            date.className - "forecast-date";
-                            card.appendChild(date);
-
-                            var temp = document.createElement("p");
-                            temp.className = "temperature";
-                            temp.textContent = temperature;
-                            card.appendChild(temp);
-
-                            var wind = document.createElement("p");
-                            wind.className = "windspeed";
-                            wind.textContent = windspeed;
-                            card.appendChild(wind);
-
-                            var humidityElement = document.createElement("p");
-                            humidityElement.className = "humidity";
-                            humidityElement.textContent = humidity;
-                            card.appendChild(humidityElement);
-
-                            return card;
-                        }
-
-                    })
-            }
-        })}
 
 
 
